0d4bfe5fb13d7c5cca084a6d7a1ea424afb9b357
Jack Rome <jar7@hw.ac.uk>
2020-02-29 18:59:55 +0000

    Implement A* maze for assignment

--- GPGame_Template_VS2019_x64/GPGame_Template_VS2019_x64/GPGame_Template_VS2019_x64.cpp
+++ GPGame_Template_VS2019_x64/GPGame_Template_VS2019_x64/GPGame_Template_VS2019_x64.cpp
@@ -4,34 +4,38 @@
 #include "graph.h"
 #include "Astar.h"
 
-constexpr bool grid[10][10] = { {0, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-								{0, 0, 1, 0, 0, 0, 0, 0, 0, 1},
-								{1, 0, 1, 0, 1, 1, 0, 1, 0, 1},
-								{1, 0, 0, 0, 1, 0, 0, 1, 1, 1},
-								{1, 0, 1, 1, 0, 0, 0, 1, 0, 1},
-								{1, 0, 1, 0, 0, 0, 0, 1, 1, 1},
-								{1, 0, 1, 0, 0, 1, 0, 0, 0, 1},
-								{1, 0, 1, 1, 1, 0, 0, 1, 1, 1},
-								{1, 0, 0, 1, 0, 0, 0, 0, 0, 1},
-								{1, 1, 1, 1, 1, 1, 1, 1, 0, 0} }; //create blocks on 1s, empty space on zeros
+constexpr int ROWS = 20;
+constexpr int COLS = 20;
+constexpr bool grid[ROWS][COLS] = { {0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+									{1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0},
+									{1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0},
+									{1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0},
+									{0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0},
+									{0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1},
+									{1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0},
+									{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0},
+									{0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+									{1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+									{0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0},
+									{0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1},
+									{0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0},
+									{0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0},
+									{1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1},
+									{0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0},
+									{0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0},
+									{0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0},
+									{0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0},
+									{0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0},
+								}; //create blocks on 1s, empty space on zeros
 
-constexpr int ROWS = 10;
-constexpr int COLS = 10;
 constexpr int MAZE_SIZE = ROWS * COLS;
+constexpr int PHYSICS_DEMO = 57;
+constexpr int PARTICLE_DEMO = 342;
+constexpr int BOIDS_DEMO = 357;
 
-struct character
-{
-	Sphere shape;
-	GLfloat pos_x = float(ROWS);
-	GLfloat pos_y = 0.5f;
-	GLfloat pos_z = float(COLS);
-	GLfloat roll = 0.0f;
-	GLfloat pitch = 0.0f;
-	GLfloat yaw = 0.0f;
+int target = -1;
 
-} myCharacter;
-
-AABB character_aabb;
+AABB camera_aabb;
 typedef struct wall
 {
 	Cube visual;
@@ -50,6 +54,10 @@ int main()
 {
 	int errorGraphics = myGraphics.Init();			// Launch window and graphics context
 	if (errorGraphics) return 0;					// Close if something went wrong...
+	cout << "Please enter the number of your destination:" << std::endl;
+	cout << "1. Physics Demo" << std::endl;
+	cout << "2. Particle Demo" << std::endl;
+	cout << "3. Boids Demo" << std::endl;
 
 	startup();										// Setup all necessary information for startup (aka. load texture, shaders, models, etc).
 
@@ -112,7 +120,7 @@ void startup() {
 	myGraphics.aspect = (float)myGraphics.windowWidth / (float)myGraphics.windowHeight;
 	myGraphics.proj_matrix = glm::perspective(glm::radians(50.0f), myGraphics.aspect, 0.1f, 1000.0f);
 
-	myGraphics.cameraPosition = glm::vec3(5.16f, 9.44f, -2.33f);
+	myGraphics.cameraPosition = glm::vec3(float(ROWS), 0.5f, float(COLS));
 	myGraphics.cameraYaw = 90.05f;
 	myGraphics.cameraPitch = -55.05f;
 
@@ -132,14 +140,12 @@ void startup() {
 		Cube c;
 		c.Load();
 		c.fillColor = glm::vec4(1.0f, 1.0f, 0.0f, 0.75f);
+		c.lineColor = glm::vec4(1.0f, 1.0f, 0.0f, 0.0f);
 		path_visuals.push_back(c);
 	}
 
-	//character
-	myCharacter.shape.Load();
-	myCharacter.shape.fillColor = glm::vec4(0.0f, 1.0f, 0.0f, 1.0f);
-	character_aabb.r = Point(myCharacter.pos_x, myCharacter.pos_y, myCharacter.pos_z);
-	character_aabb.r = Point(0.4, 0.5f, 0.4f);
+	camera_aabb.r = Point(myGraphics.cameraPosition.x, myGraphics.cameraPosition.y, myGraphics.cameraPosition.z);
+	camera_aabb.r = Point(0.3f, 0.3f, 0.3f);
 	// Optimised Graphics
 	myGraphics.SetOptimisations();        // Cull and depth testing
 
@@ -154,9 +160,9 @@ void createMaze()
 		for (int j = 0; j < COLS; j++)
 		{
 			glm::vec3 pos;
-			pos.x = float(ROWS - i);
+			pos.x = float(COLS - j);
 			pos.y = 0.5f;
-			pos.z = float(COLS - j);
+			pos.z = float(ROWS - i);
 			const bool create_wall = grid[i][j];
 			if (create_wall)
 			{
@@ -167,7 +173,7 @@ void createMaze()
 				w.pos = pos;
 				AABB aabb;
 				aabb.c = Point(pos.x, pos.y, pos.z);
-				aabb.r = Point(0.5f, 0.5f, .5f);
+				aabb.r = Point(0.5f, 0.5f, 0.5f);
 				w.aabb = aabb;
 				inner_walls.push_back(w);
 			}
@@ -181,11 +187,9 @@ void createMaze()
 			k++;
 		}
 	}
-	//cout << "Size " << graph.getNumber() << "\n";
 	//Connect cells
 	for (auto p : graph.getVertices())
 	{
-		Vertex v = p.second;
 		int frm = p.first;
 		int to = frm + ROWS;
 		if ((frm + ROWS) <= (MAZE_SIZE - 1) && graph.checkID(to))
@@ -200,17 +204,16 @@ void createMaze()
 	}
 	//for (auto p : graph.getVertices())
 	//{
-	//	Vertex vertex = p.second;
-	//	for (auto v : vertex.getConnections())
+	//	Vertex v = p.second;
+	//	cout << std::endl << v.getID() << ": ";
+	//	for (auto adjacent : v.getConnections())
 	//	{
-	//		int id = v.first;
-	//		auto pos = graph.getVertex(id).getPosition();
-	//		cout << "(" << pos.x << ", " << pos.y << ")";
-	//		cout << id << ", ";
+	//		auto a = adjacent.first;
+	//		cout << a << ", ";
 	//	}
-	//	cout << "\n";
 	//}
-	//outer walls
+		
+	//Outer walls
 	for (int i = 0; i < ROWS + 1; i++)
 	{
 		glm::vec3 pos;
@@ -260,12 +263,47 @@ void updateCamera() {
 
 	myGraphics.cameraFront = glm::normalize(front);
 
+	glm::vec3 next_pos = myGraphics.cameraPosition;
+
 	// Update movement using the keys
-	GLfloat cameraSpeed = 1.0f * deltaTime;
-	if (keyStatus[GLFW_KEY_W]) myGraphics.cameraPosition += cameraSpeed * myGraphics.cameraFront;
-	if (keyStatus[GLFW_KEY_S]) myGraphics.cameraPosition -= cameraSpeed * myGraphics.cameraFront;
-	if (keyStatus[GLFW_KEY_A]) myGraphics.cameraPosition -= glm::normalize(glm::cross(myGraphics.cameraFront, myGraphics.cameraUp)) * cameraSpeed;
-	if (keyStatus[GLFW_KEY_D]) myGraphics.cameraPosition += glm::normalize(glm::cross(myGraphics.cameraFront, myGraphics.cameraUp)) * cameraSpeed;
+	GLfloat cameraSpeed = 2.0f * deltaTime;
+	if (keyStatus[GLFW_KEY_W]) next_pos += cameraSpeed * myGraphics.cameraFront;
+	if (keyStatus[GLFW_KEY_S]) next_pos -= cameraSpeed * myGraphics.cameraFront;
+	if (keyStatus[GLFW_KEY_A]) next_pos -= glm::normalize(glm::cross(myGraphics.cameraFront, myGraphics.cameraUp)) * cameraSpeed;
+	if (keyStatus[GLFW_KEY_D]) next_pos += glm::normalize(glm::cross(myGraphics.cameraFront, myGraphics.cameraUp)) * cameraSpeed;
+	
+	next_pos.y = 0.5f; //fixed height;
+	camera_aabb.c = Point(next_pos.x, next_pos.y, next_pos.z);
+
+	bool collision = false;
+	for (auto wall : inner_walls)
+	{
+		if (isCollisionSphere(camera_aabb, wall.aabb))
+		{
+			collision = true;
+			break;
+		}
+	}
+	if (not collision) {
+
+		for (auto wall : outer_walls)
+		{
+			if (isCollisionSphere(camera_aabb, wall.aabb))
+			{
+				collision = true;
+				break;
+			}
+		}
+	}
+	if (!collision)
+	{
+		//update camera
+		myGraphics.cameraPosition = next_pos;
+	}
+	else
+	{
+		camera_aabb.c = Point{ myGraphics.cameraPosition.x, myGraphics.cameraPosition.y, myGraphics.cameraPosition.z };
+	}
 
 	// IMPORTANT PART
 	// Calculate my view matrix using the lookAt helper function
@@ -291,7 +329,7 @@ void updateSceneElements() {
 		// Calculate Cube position
 		glm::mat4 mv_matrix_cube =
 			glm::translate(glm::vec3(wall.pos.x, wall.pos.y, wall.pos.z)) *
-			glm::scale(glm::vec3(1.0f, 0.5f, 1.0f))*
+			glm::scale(glm::vec3(1.0f, 1.0f, 1.0f))*
 			glm::mat4(1.0f);
 		wall.visual.mv_matrix = myGraphics.viewMatrix * mv_matrix_cube;
 		wall.visual.proj_matrix = myGraphics.proj_matrix;
@@ -302,7 +340,7 @@ void updateSceneElements() {
 		// Calculate Cube position
 		glm::mat4 mv_matrix_cube =
 			glm::translate(glm::vec3(wall.pos.x, wall.pos.y, wall.pos.z)) *
-			glm::scale(glm::vec3(1.0f, 0.5f, 1.0f)) *
+			glm::scale(glm::vec3(1.0f, 1.0f, 1.0f)) *
 			glm::mat4(1.0f);
 		wall.visual.mv_matrix = myGraphics.viewMatrix * mv_matrix_cube;
 		wall.visual.proj_matrix = myGraphics.proj_matrix;
@@ -314,29 +352,40 @@ void updateSceneElements() {
 		glm::scale(glm::vec3(1000.0f, 0.001f, 1000.0f)) *
 		glm::mat4(1.0f);
 	myFloor.proj_matrix = myGraphics.proj_matrix;
-
-	GLfloat x = 0.0f;
-	GLfloat z = 0.0f;
-	GLfloat speed = 0.05f;
-
-	if (keyStatus[GLFW_KEY_UP]) z += speed;
-	if (keyStatus[GLFW_KEY_DOWN]) z -= speed;
-	if (keyStatus[GLFW_KEY_LEFT]) x += speed;
-	if (keyStatus[GLFW_KEY_RIGHT]) x -= speed;
-	moveCharacter(x, 0.0f, z);
-
+	
+	bool new_path = false;
 	// Calculate floor position and resize
-	glm::vec3 cell_pos;
-	cell_pos.x = roundf(myCharacter.pos_x);
-	cell_pos.y = 0.5f;
-	cell_pos.z = roundf(myCharacter.pos_z);
-
-	if (cell_pos != prev_cell_pos)
+	if (keyStatus[GLFW_KEY_1]) 
 	{
-		cout << "New path";
-		path.clear();
-		int start = (ROWS-cell_pos.x)*ROWS + (COLS-cell_pos.z); 
-		a_star_search(graph, start, MAZE_SIZE - 1, path);
+		target = PHYSICS_DEMO;
+		new_path = true;
+	}
+	if (keyStatus[GLFW_KEY_2])
+	{
+		target = PARTICLE_DEMO;
+		new_path = true;
+
+	}
+	if (keyStatus[GLFW_KEY_3])
+	{
+		target = BOIDS_DEMO;
+		new_path = true;
+	}
+
+	glm::vec3 cell_pos;
+	cell_pos.x = roundf(myGraphics.cameraPosition.x);
+	cell_pos.y = 0.5f;
+	cell_pos.z = roundf(myGraphics.cameraPosition.z);
+	
+	if ((cell_pos != prev_cell_pos) or new_path)
+	{
+		//cout << "New path";
+		if (target != -1)
+		{
+			path.clear();
+			int start = (ROWS - cell_pos.z) * ROWS + (COLS - cell_pos.x);
+			a_star_search(graph, start, target, path);
+		}
 		prev_cell_pos = cell_pos;
 	}
 
@@ -355,7 +404,7 @@ void updateSceneElements() {
 			auto pos = p.getPosition();
 			c.mv_matrix = myGraphics.viewMatrix *
 				glm::translate(glm::vec3(pos.x, 0.0f, pos.y)) *
-				glm::scale(glm::vec3(1.0f, 0.25f, 1.0f)) *
+				glm::scale(glm::vec3(1.0f, 0.01f, 1.0f)) *
 				glm::mat4(1.0f);
 			c.proj_matrix = myGraphics.proj_matrix;
 		}
@@ -369,72 +418,6 @@ void updateSceneElements() {
 		}
 	}
 	if (glfwWindowShouldClose(myGraphics.window) == GL_TRUE) quit = true; // If quit by pressing x on window.
-}
-
-void moveCharacter(const GLfloat x, const GLfloat y, const GLfloat z)
-{
-	//calculate displacements
-	GLfloat new_x = myCharacter.pos_x + x;
-	GLfloat new_y = myCharacter.pos_y + y;
-	GLfloat new_z = myCharacter.pos_z + z;
-
-	GLfloat new_roll = myCharacter.roll + z;
-	GLfloat new_pitch = myCharacter.pitch + x;
-	GLfloat new_yaw = myCharacter.yaw + y;
-
-	character_aabb.c = Point(new_x, new_y, new_z);
-
-	bool collision = false;
-	for (auto wall: inner_walls)
-	{
-		if (isCollisionSphere(character_aabb, wall.aabb))
-		{
-			collision = true;
-			break;
-		}
-	}
-	if (not collision) {
-
-		for (auto wall : outer_walls)
-		{
-			if (isCollisionSphere(character_aabb, wall.aabb))
-			{
-				collision = true;
-				break;
-			}
-		}
-	}
-	if (collision)
-	{
-		new_x = myCharacter.pos_x;
-		new_y = myCharacter.pos_y;
-		new_z = myCharacter.pos_z;
-
-		new_roll = myCharacter.roll;
-		new_pitch = myCharacter.pitch;
-		new_yaw = myCharacter.yaw;
-		character_aabb.c = Point(new_x, new_y, new_z);
-	}
-	else
-	{
-		//update Character
-		myCharacter.pos_x = new_x;
-		myCharacter.pos_y = new_y;
-		myCharacter.pos_z = new_z;
-		myCharacter.roll = new_roll;
-		myCharacter.pitch = new_pitch;
-		myCharacter.yaw = new_yaw;
-	}
-
-	//translate and rotate accordingly
-	glm::mat4 mv_matrix_character =
-		glm::translate(glm::vec3(new_x, new_y, new_z)) *
-		glm::rotate(new_roll, glm::vec3(1.0f, 0.0f, 0.0f)) *
-		glm::rotate(new_pitch, glm::vec3(0.0f, 0.0f, -1.0f)) *
-		glm::rotate(new_yaw, glm::vec3(0.0f, 1.0f, 0.0f)) *
-		glm::mat4(1.0f);
-	myCharacter.shape.mv_matrix = myGraphics.viewMatrix * mv_matrix_character;
-	myCharacter.shape.proj_matrix = myGraphics.proj_matrix;
 }
 
 void renderScene() {
@@ -454,7 +437,6 @@ void renderScene() {
 	{
 		p.Draw();
 	}
-	myCharacter.shape.Draw();
 	active_cell.Draw();
 }
 
@@ -475,10 +457,10 @@ void onKeyCallback(GLFWwindow* window, int key, int scancode, int action, int mo
 
 	// toggle showing mouse.
 	if (keyStatus[GLFW_KEY_M]) {
-		cout << "Camera up:" << myGraphics.cameraUp.x << " " << myGraphics.cameraUp.y << " " << myGraphics.cameraUp.z << "\n";
+	/*	cout << "Camera up:" << myGraphics.cameraUp.x << " " << myGraphics.cameraUp.y << " " << myGraphics.cameraUp.z << "\n";
 		cout << "Camera pitch:" << myGraphics.cameraPitch << "\n";
 		cout << "Camera yaw:" << myGraphics.cameraYaw << "\n";
-		cout << "Camera pos:" << myGraphics.cameraPosition.x << " " << myGraphics.cameraPosition.y << " " << myGraphics.cameraPosition.z << "/n";
+		cout << "Camera pos:" << myGraphics.cameraPosition.x << " " << myGraphics.cameraPosition.y << " " << myGraphics.cameraPosition.z << "/n";*/
 		mouseEnabled = !mouseEnabled;
 		myGraphics.ToggleMouse();
 	}
